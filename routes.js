c10753351b2c96f56f312fdf51c930ba945b5b94
Torres-Brayan <130689664+Torres-Brayan@users.noreply.github.com>
2023-06-01 17:29:55 -0500

    version 2.0 Co-authored-by: Nathan Vititoe <Nathanvititoe@users.noreply.github.com>

---
+++ routes.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var petPath = path.join(__dirname, 'pets.json');
+
+console.log(petPath[1]);
+var routes = {
+    '/0' : function(req, res) {
+
+    },
+    '/1' : function(req,) {
+
+    }
+};
